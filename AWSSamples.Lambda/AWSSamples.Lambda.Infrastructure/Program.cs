46b52efd25cf8879a90dc4c25b57ea6190558d76
Tim Grannen <grannendev@gmail.com>
2023-01-14 12:19:33 -0500

    Remove testing pulumi function config

--- AWSSamples.Lambda/AWSSamples.Lambda.Infrastructure/Program.cs
+++ AWSSamples.Lambda/AWSSamples.Lambda.Infrastructure/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Pulumi;
 using System.Collections.Generic;
-using AWSSamples.Lambda.Infrastructure.Helpers;
 using Aws = Pulumi.Aws;
 
 return await Deployment.RunAsync(() =>
@@ -8,38 +7,6 @@ return await Deployment.RunAsync(() =>
     var config = new Config();
     var domain = config.Require("domain");
     var nameBase = "aws-samples-lambda";
-    var myStack = new MyStack(nameBase, @"..\AWSSamples.Lambda.Web\bin\Release\net6.0\AWSSamples.Lambda.Web.zip");
-//
-//     var iamForLambda = new Aws.Iam.Role($"{nameBase}-iamForLambda", new()
-//     {
-//         Name = $"{nameBase}-exampleFunction",
-//         AssumeRolePolicy = @"{
-//   ""Version"": ""2012-10-17"",
-//   ""Statement"": [
-//     {
-//       ""Action"": ""sts:AssumeRole"",
-//       ""Principal"": {
-//         ""Service"": ""lambda.amazonaws.com""
-//       },
-//       ""Effect"": ""Allow"",
-//       ""Sid"": """"
-//     }
-//   ]
-// }
-// ",
-//     });
-//     
-//     // Create an AWS resource (S3 Bucket)
-//     var bucket = new Bucket($"{nameBase}-example-bucket");
-//
-//     var function = new Aws.Lambda.Function($"{nameBase}-exampleFunction", new()
-//     {
-//         Name = $"{nameBase}-exampleFunction",
-//         S3Bucket = bucket.Arn,
-//         Role = iamForLambda.Arn,
-//         Handler = "AWSSamples.Lambda.Web",
-//         Runtime = "dotnet6",
-//     });
 
     var cert = Aws.Acm.GetCertificate.Invoke(new()
     {
@@ -52,16 +19,16 @@ return await Deployment.RunAsync(() =>
         ProtocolType = "HTTP",
     });
 
-    var integration = new Aws.ApiGatewayV2.Integration($"{nameBase}-gateway-integration", new()
-    {
-        ApiId = api.Id,
-        IntegrationType = "AWS_PROXY",
-        ConnectionType = "INTERNET",
-        Description = "Lambda example",
-        IntegrationMethod = "POST",
-        IntegrationUri = myStack.LambdaHelloWorldFunction.InvokeArn,
-        PassthroughBehavior = "WHEN_NO_MATCH",
-    });
+    // var integration = new Aws.ApiGatewayV2.Integration($"{nameBase}-gateway-integration", new()
+    // {
+    //     ApiId = api.Id,
+    //     IntegrationType = "AWS_PROXY",
+    //     ConnectionType = "INTERNET",
+    //     Description = "Lambda example",
+    //     IntegrationUri = myStack.LambdaHelloWorldFunction.InvokeArn,
+    //     IntegrationMethod = "POST",
+    //     PassthroughBehavior = "WHEN_NO_MATCH",
+    // });
 
     var stage = new Aws.ApiGatewayV2.Stage($"{nameBase}-gateway-stage", new()
     {
@@ -69,12 +36,12 @@ return await Deployment.RunAsync(() =>
         ApiId = api.Id,
     });
 
-    var route = new Aws.ApiGatewayV2.Route($"{nameBase}-gateway-route", new()
-    {
-        ApiId = api.Id,
-        RouteKey = "$default",
-        Target = integration.Id.Apply(id => $"integrations/{id}")
-    });
+    // var route = new Aws.ApiGatewayV2.Route($"{nameBase}-gateway-route", new()
+    // {
+    //     ApiId = api.Id,
+    //     RouteKey = "$default",
+    //     Target = integration.Id.Apply(id => $"integrations/{id}")
+    // });
 
     var domainName = new Aws.ApiGatewayV2.DomainName($"{nameBase}-gateway-domainname", new()
     {
